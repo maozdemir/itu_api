f2a5685d25b5cdd8bb1b6b0ffe160a715e6111eb
impulsivus <alperozdemir@mail.com>
2022-05-15 17:01:41 +0300

    ITU_API/db.php: Add documentation

--- db.php
+++ db.php
@@ -11,12 +11,14 @@ setlocale(LC_TIME, 'trk.UTF-8');
 class DatabaseMgr
 {
     /**
+     * Database connection
      * @return mysqli $mysqli_connection
      */
 
     static private function dbconnection()
     {
-        $mysqli = new mysqli("localhost", "root", "", "itu_api");
+        //$mysqli = new mysqli("localhost", "root", "", "itu_api");
+        $mysqli = new mysqli("localhost", "root", "", "itu_database");
         if ($mysqli->connect_errno) {
             printf("Connect failed: %s\n", $mysqli->connect_error);
             exit();
@@ -25,7 +27,8 @@ class DatabaseMgr
     }
 
     /**
-     * @param Tur $turObject
+     * Add new course to database
+     * @param Course $course
      * @return boolean $success
      */
 
@@ -75,6 +78,12 @@ class DatabaseMgr
             return false;
         }
     }
+
+    /**
+     * Get course data
+     * @param string $id
+     * @return array
+     */
     static public function get_course($id)
     {
 
@@ -93,6 +102,11 @@ class DatabaseMgr
         }
     }
 
+    /**
+     * Get course data for RESTful API
+     * @param int $id
+     * @return array
+     */
     static public function course_quota_provider($id)
     {
         $course_data = DatabaseMgr::get_course($id);
@@ -104,6 +118,12 @@ class DatabaseMgr
         return $course_data_proc;
     }
 
+    /**
+     * Get location data from name
+     * @param int $location
+     * @return array
+     */
+
     static public function get_location($location)
     {
 
@@ -114,13 +134,20 @@ class DatabaseMgr
                     return $row;
                 }
             } else {
-                return "ERR";
+                return false;
             }
         } else {
-            printf("ERR: %s\n", $db->error);
-            return "ERR";
+            return false;
         }
     }
+
+    /**
+     * Get location name from location id
+     *
+     * @param int $location_id
+     * @return boolean|array Returns false if location id is not found, otherwise returns location name
+     */
+
     static public function get_location_name($location_id)
     {
 
@@ -131,71 +158,87 @@ class DatabaseMgr
                     return $row;
                 }
             } else {
-                return "ERR";
+                return false;
             }
         } else {
-            printf("ERR: %s\n", $db->error);
-            return "ERR";
+            return false;
         }
     }
+
+    /**
+     * Add new location to database.
+     * @param int $location Location name
+     * @return boolean $success
+     */
     static public function add_location($location)
     {
         $connection = DatabaseMgr::dbconnection();
         if ($result = $connection->query("SELECT * FROM `locations` WHERE `name` = '$location'")) {
             if ($result->num_rows > 0) {
-                return "Aleready exists!";
+                return false;
             } else {
                 if ($location != '') {
                     if ($connection->query("INSERT INTO `locations` (`name`) VALUES ('$location')")) {
                         return true;
                     } else {
-                        printf("HATA: %s\n", $connection->error);
                         return false;
                     }
                 }
             }
         } else {
-            printf("ERR: %s\n", $connection->error);
-            return "ERR";
+            return false;
         }
     }
+
+    /**
+     * Add new building to database.
+     * @param int $location Location ID
+     * @param string $code Building Code
+     * @param string $name_tr Building Name (Turkish)
+     * @return boolean $success
+     */
+
     static public function add_building($location, $code, $name_tr)
     {
         $connection = DatabaseMgr::dbconnection();
         if ($result = $connection->query("SELECT * FROM `buildings` WHERE `code` = '$code'")) {
             if ($result->num_rows > 0) {
-                return "Aleready exists!";
+                return false;
             } else {
                 if ($location != '') {
                     if ($connection->query("INSERT INTO `buildings` (`code`,`name_tr`,`location`) VALUES ('$code','$name_tr','$location')")) {
                         return true;
                     } else {
-                        printf("HATA: %s\n", $connection->error);
                         return false;
                     }
                 }
             }
         } else {
-            printf("ERR: %s\n", $connection->error);
-            return "ERR";
+            return false;
         }
     }
 
-    static public function get_building($bld_id)
+    /**
+     * Returns building data by building ID. If building ID is not found, returns false.
+     * @param int $building_id
+     * @return array $building_data
+     */
+
+    static public function get_building($building_id)
     {
 
         $db = DatabaseMgr::dbconnection();
-        if ($result = $db->query("SELECT * FROM `buildings` WHERE `id` = '$bld_id'")) {
+        if ($result = $db->query("SELECT * FROM `buildings` WHERE `id` = '$building_id'")) {
             if ($result->num_rows > 0) {
                 while ($row = $result->fetch_assoc()) {
                     return $row;
                 }
             } else {
-                return "ERR";
+                return false;
             }
         } else {
             printf("ERR: %s\n", $db->error);
-            return "ERR";
+            return false;
         }
     }
 }
